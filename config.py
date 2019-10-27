f48b66aca4d16b280a60a9904851f3c81355b239
Hunter Kuffel <hkuffel@alumni.nd.edu>
2019-10-27 15:00:38 -0400

    add flask-pymongo and atlas db

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 import os
 
-API_KEY = os.environ.get('API_KEY') or '2d738d06a628d81d26514e8564559d08'
+API_KEY = os.environ.get('API_KEY')
+
+MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://hk:jXmCTbdxj1c8HXhP@cluster0-htedx.mongodb.net/trips_db?retryWrites=true&w=majority'
