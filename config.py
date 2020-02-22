5540eee831750b80ab8d72276d11622d80c5335c
Hunter Kuffel <hkuffel@alumni.nd.edu>
2020-02-21 20:52:55 -0500

    add bar graph

--- config.py
+++ config.py
@@ -2,4 +2,8 @@ import os
 
 ***REMOVED***
 
-MANGO_URI = os.environ.get('MANGO_URI') or 'mongodb+srv://hk:jXmCTbdxj1c8HXhP@cluster0-htedx.mongodb.net/trips_db?retryWrites=true&w=majority'
+MANGO_URI = os.environ.get('MANGO_URI') or 'mongodb+srv://hunter:C8RKnWrxIgvxoXSD@cluster0-h8le0.mongodb.net/test?retryWrites=true&w=majority'
+
+BROKER_URL=os.environ['REDIS_URL']
+
+CELERY_RESULT_BACKEND=os.environ['REDIS_URL']
